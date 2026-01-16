b5dfaeed76924b3fd98396c19e6b597249900550
sotka12211-a11y <sotka12211@gmail.com>
2026-01-16 13:55:35 +0500

    Create lots.ts

---
+++ backend/src/lots.ts
@@ -0,0 +1,20 @@
+export const lots = [
+  {
+    id: 1,
+    title: "🚗 Tesla Model X",
+    price: 1000,
+    owner: null
+  },
+  {
+    id: 2,
+    title: "🏠 Luxury House",
+    price: 5000,
+    owner: null
+  },
+  {
+    id: 3,
+    title: "🏝️ Private Villa",
+    price: 10000,
+    owner: null
+  }
+];
